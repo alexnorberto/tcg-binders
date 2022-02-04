bc8b3c10ea9d72ae67e30101b0d6a260cbaf4993
alexnorberto <alexsandronorberto@gmail.com>
2022-02-04 13:52:22 -0300

    comment some code

--- src/app/services/tcg-api.service.ts
+++ src/app/services/tcg-api.service.ts
@@ -3,9 +3,15 @@ import { HttpClient, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpR
 import { Observable } from 'rxjs';
 import {environment} from "../../environments/environment";
 
+/**
+ * Setup consts for tcg api
+ */
 const tcgApiUrl = "https://api.pokemontcg.io/v2/cards";
 const headers = new HttpHeaders().set('x-api-key', environment.tcgAPI);
 
+/**
+ * This services deal with tcg api requests
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -15,16 +21,29 @@ export class TcgApiService {
     private http: HttpClient
   ) { }
 
+  /**
+   * Create header for get api data
+   * @param headers
+   */
   createAuthorizationHeader(headers: HttpHeaders) {
     headers.append('Content-Type', 'application/json');
     headers.append('x-api-key', '7cf869e2-9e98-421a-b24c-e5b2e29ca855');
     console.log(headers)
   }
 
+  /**
+   * Get list of cards with default pagination (20)
+   */
   get(): Observable<any>{
     return this.http.get<any>(tcgApiUrl+"?pageSize=20",{ headers: headers });
   }
 
+  /**
+   * Get a filtered list of cards from API based on some filters
+   * @param name
+   * @param page
+   * @param pageSize
+   */
   simpleCardSearch(name,page = 1,pageSize = 20): Observable<any>{
     if(name == "") {
       return this.http.get<any>( `${tcgApiUrl}?page=${page}&pageSize=${pageSize}`,{ headers: headers });
@@ -34,6 +53,10 @@ export class TcgApiService {
 
   }
 
+  /**
+   * Get a single card from API passing an ID
+   * @param id
+   */
   getACard(id): Observable<any>{
     return this.http.get<any>(tcgApiUrl+"/"+id,{ headers: headers });
   }
--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -3,18 +3,33 @@ import { BehaviorSubject } from 'rxjs';
 import {UserDataModel} from "../shared/models/user-data.model";
 import {AuthService} from "./auth.service";
 
+/**
+ * Service to deal with user data
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class UserDataService {
 
   private messageSource = new BehaviorSubject(new UserDataModel());
+
+  /**
+   * Observable to the current user data
+   */
   currentUserData = this.messageSource.asObservable();
 
   changeUserData(userData: UserDataModel) {
     this.messageSource.next(userData)
   }
 
+  /**
+   * Given user data, group this data into one single object
+   * @param userData
+   * @param name
+   * @param email
+   * @param cards
+   * @param collection
+   */
   setUserData(userData = null, name = null, email = null, cards = null, collection = null) {
     console.log("Entradas do SetUserData",userData,name,email,cards,collection)
     let response: UserDataModel;
